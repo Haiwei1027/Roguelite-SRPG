beae4a90f57280f6a96facd734a40feb7534691a
Haiwei1027 <71439492+Haiwei1027@users.noreply.github.com>
2023-12-26 16:19:10 +0000

    Init demo level

---
+++ Assets/Scripts/Team.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour
+{
+    List<Spawner> spawners;
+
+    private void Start()
+    {
+        foreach (Spawner spawner in spawners)
+        {
+
+        }
+    }
+}
